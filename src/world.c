4b1763994cebd876bb6c5a6bf6a5b4dddcffe271
emecdelam <kiwi.thefruit0000@gmail.com>
2025-03-22 13:54:32 +0100

    changing function signature

--- src/world.c
+++ src/world.c
@@ -1,11 +1,11 @@
 #include "defines.h"
-#include <stdbool.h>
 #include "log.h"
 
 
-bool p_draw_player(float x, float y){
+void p_draw_player(float x, float y){
     log_info("drawing player at pos %f, %f", x, y);
     DrawCircle(x, y, player_radius, player_color);
+    
 }
 
 
