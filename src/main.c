c636e29896db68974fac2140ea282ef37fd47a61
De Raadt Tristan <tristan.deraadt@student.uclouvain.be>
2025-03-22 17:59:40 +0100

    Base gameplay + bug fixes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <raylib.h>
 #include "geo.h"
 #include "world.h"
+#include "player.h"
+
+PlayerPrey* players[2];
+
 int main(void)
 {
     // Initialization
@@ -13,9 +17,27 @@ int main(void)
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
+
+    // Init Players
+    Circle c1, c2; 
+    c1.radius = 30;
+    c2.radius = 30;
+    c1.pos.x = 200; 
+    c1.pos.y = 200;
+    c2.pos.x = 500;
+    c2.pos.y = 200;
+
+    players[0] = p_player_prey_create(0, 200, &c1);
+    players[1] = p_player_prey_create(0, 200, &c2);
+
+    // -------------------------------------------------------------------------------------
+
+    float time;
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+
+        time = GetFrameTime();
         // Update
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
@@ -26,7 +48,19 @@ int main(void)
         BeginDrawing();
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawCircle((int)players[0]->hitbox.pos.x, (int)players[0]->hitbox.pos.y, players[0]->hitbox.radius, RED);
+            DrawCircle((int)players[1]->hitbox.pos.x, (int)players[1]->hitbox.pos.y, players[1]->hitbox.radius, DARKBLUE);
+
+            Position cursor;
+            cursor.x = (float)GetMouseX();
+            cursor.y = (float)GetMouseY();
+            //trucs
+    
+            if(IsKeyDown(KEY_W))
+                p_player_prey_move(players[0],&cursor,time);
+            //p_player_prey_move(players[1],&cursor,time);
+
+            //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
             
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -35,6 +69,8 @@ int main(void)
     // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
+    p_player_prey_free(players[0]);
+    p_player_prey_free(players[1]);   
     //--------------------------------------------------------------------------------------
 
     return 0;
--- src/player.c
+++ src/player.c
@@ -1,10 +1,34 @@
 #include "player.h"
 
-void p_paint_regen(Player_hunter *hunter, clock_t time)
+void p_paint_regen(PlayerHunter *hunter, float time)
 {
-    hunter->paint_balls += hunter->paint_per_s * (time / CLOCKS_PER_SEC);
+    hunter->paint_balls += hunter->paint_per_s * time;
     if(hunter->paint_balls >= hunter->paint_balls_max)
         hunter->paint_balls = hunter->paint_balls_max;
 }
 
+PlayerPrey* p_player_prey_create(unsigned int iD, float speed, Circle *hitbox)
+{
+    PlayerPrey *player = (PlayerPrey *) malloc(sizeof(PlayerPrey));
+    player->iD = iD;
+    player->speed = speed;
+    player->hitbox.pos.x = hitbox->pos.x;
+    player->hitbox.pos.y = hitbox->pos.y;
+    player->hitbox.radius = hitbox->radius;
+    return player;
+}
 
+void p_player_prey_free(PlayerPrey *player)
+{
+    free(player);
+}
+
+void p_player_prey_move(PlayerPrey *player, Position *cursor, float time)
+{
+    float normal = p_fast_inverse_sqrt((cursor->x - player->hitbox.pos.x) * (cursor->x - player->hitbox.pos.x) + (cursor->y - player->hitbox.pos.y) * (cursor->y - player->hitbox.pos.y));
+    if(normal < 0.3)
+    {
+        player->hitbox.pos.x += player->speed * time * normal * (cursor->x - player->hitbox.pos.x);
+        player->hitbox.pos.y += player->speed * time * normal * (cursor->y - player->hitbox.pos.y);
+    }
+}
